90c7acb99d301d4c3af4a3d746a3c8ce12bfe980
Sunbalcony <1141789386@qq.com>
2021-09-17 16:37:22 +0800

    create

---
+++ 01/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"core/framework"
+	"fmt"
+	"net/http"
+)
+//所有的框架代码都存放在 framework 文件夹中，而所有的示例业务代码都存放在 framework 文件夹之外
+func main() {
+	server := &http.Server{
+		Handler: framework.NewCore(),
+		Addr: ":8080",
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
