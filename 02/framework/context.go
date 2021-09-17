9979685cd1eeb7bc7a72f5c1848bb1913489733f
NotByChance <160632460@qq.com>
2021-09-17 18:26:39 +0800

    create

--- 02/framework/context.go
+++ 02/framework/context.go
@@ -7,24 +7,46 @@ import (
 )
 
 type Context struct {
-	request *http.Request
+	request        *http.Request
 	responseWriter http.ResponseWriter
-	ctx context.Context
+	ctx            context.Context
 	//是否超时标记位
 	hasTimeout bool
 	//写保护机制
 	writerMux *sync.Mutex
-
 }
 
-func NewContext(r *http.Request,w http.ResponseWriter) *Context {
+func NewContext(r *http.Request, w http.ResponseWriter) *Context {
 	return &Context{
-		request: r,
+		request:        r,
 		responseWriter: w,
-		ctx: r.Context(),
-		writerMux: &sync.Mutex{},
-
+		ctx:            r.Context(),
+		writerMux:      &sync.Mutex{},
 	}
 
 }
 
+//基础函数
+
+// WriteMux 加锁保护
+func (ctx *Context) WriteMux() *sync.Mutex {
+	return ctx.writerMux
+
+}
+
+// GetRequest 获取请求
+func (ctx *Context) GetRequest() *http.Request {
+	return ctx.request
+
+}
+
+// GetResponse 获取响应
+func (ctx *Context) GetResponse() http.ResponseWriter {
+	return ctx.responseWriter
+
+}
+
+
+func (ctx *Context) SetHandler() {
+
+}
