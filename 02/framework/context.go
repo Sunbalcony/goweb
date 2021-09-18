8a36ec8fe09b8e27ff5d7ca1aaf722e5efc63d17
Sunbalcony <160632460@qq.com>
2021-09-18 08:32:16 +0800

    create

--- 02/framework/context.go
+++ 02/framework/context.go
@@ -4,27 +4,88 @@ import (
 	"context"
 	"net/http"
 	"sync"
+	"time"
 )
 
 type Context struct {
-	request *http.Request
+	request        *http.Request
 	responseWriter http.ResponseWriter
-	ctx context.Context
+	ctx            context.Context
+	handler        ControllerHandler
 	//是否超时标记位
 	hasTimeout bool
 	//写保护机制
 	writerMux *sync.Mutex
-
 }
 
-func NewContext(r *http.Request,w http.ResponseWriter) *Context {
+func NewContext(r *http.Request, w http.ResponseWriter) *Context {
 	return &Context{
-		request: r,
+		request:        r,
 		responseWriter: w,
-		ctx: r.Context(),
-		writerMux: &sync.Mutex{},
-
+		ctx:            r.Context(),
+		writerMux:      &sync.Mutex{},
 	}
 
 }
 
+//基础函数
+
+func (ctx *Context) WriterMux() *sync.Mutex {
+	return ctx.writerMux
+
+}
+
+func (ctx *Context) GetRequest() *http.Request {
+	return ctx.request
+
+}
+
+func (ctx *Context) GetResponse() http.ResponseWriter {
+	return ctx.responseWriter
+}
+
+func (ctx *Context) SetHandler(handler ControllerHandler) {
+	ctx.handler = handler
+}
+
+func (ctx *Context) SetHasTimeout() {
+	ctx.hasTimeout = true
+}
+
+func (ctx *Context) HasTimeout() bool {
+	return ctx.hasTimeout
+}
+
+//
+
+func (ctx *Context) BaseContext() context.Context {
+	return ctx.request.Context()
+
+}
+
+func (ctx *Context) Deadline() (deadline time.Time, ok bool) {
+	return ctx.BaseContext().Deadline()
+
+}
+
+func (ctx *Context) Done() <-chan struct{} {
+	return ctx.BaseContext().Done()
+
+}
+
+func (ctx *Context) Err() error {
+	return ctx.BaseContext().Err()
+}
+
+func (ctx *Context) Value(key interface{}) interface{} {
+	return ctx.BaseContext().Value(key)
+
+}
+
+func (ctx *Context) QueryAll() map[string][]string {
+	if ctx.request != nil {
+		return map[string][]string(ctx.request.URL.Query())
+	}
+	return map[string][]string{}
+
+}
